7302fe508728042a79d9f4c1bfac05555ac12c35
Rahul <kumarrahul18699@gmail.com>
2024-01-29 13:41:32 +0530

    completing day01

---
+++ 01_Day_introduction/exercises/datatypes.js
@@ -0,0 +1,37 @@
+//5
+console.log(typeof name);
+console.log(typeof married);
+console.log(typeof status);
+console.log(typeof number);
+console.log(typeof null);
+
+// 6
+let copanyName;
+let yearOfStarting;
+let growthRate;
+let numberofEmployees;
+
+// 7
+copanyName = 'mahindra';
+yearOfStarting = 1960;
+growthRate= 88;
+numberofEmployees= 200000;
+
+// 8
+
+let firstName ="Rahul";
+let lastName = "Gupta";
+let maritalStatus = false;
+let country= "India";
+let age= 25;
+
+
+// 9
+let firstName2= "Mohit", lastName2= "Gupta", maritalStatus2= false, country2= "India";
+
+//10
+let myAge=25;
+let yourAge=30;
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
+
---
+++ 01_Day_introduction/exercises/variable.js
@@ -0,0 +1,18 @@
+//1
+//comments can make code readable
+
+//2
+// Welcome to 30DaysOfJavaScript
+
+//3
+/* comments can make readable, easy to reuse and informative */
+
+//4
+let name="Rahul";
+let married=false;
+let number=null;
+let status;
+
+
+
+
